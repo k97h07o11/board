81825b3032425089100349ca7e0dd44bb0f460e4
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-06-01 03:36:43 +0900

    :fire: 댓글 Controller 클래스 레벨 @RequestMapping 제거

--- backend/src/main/java/com/example/board/comment/controller/CommentController.java
+++ backend/src/main/java/com/example/board/comment/controller/CommentController.java
@@ -2,12 +2,10 @@ package com.example.board.comment.controller;
 
 import com.example.board.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/comments")
 public class CommentController {
 
     private final CommentService commentService;
