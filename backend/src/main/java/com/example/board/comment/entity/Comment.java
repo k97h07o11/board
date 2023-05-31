1370c2aaf73b2928fd9c9aa43687d26534e1f9a5
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-06-01 02:48:26 +0900

    :sparkles: 댓글 Entity 추가

---
+++ backend/src/main/java/com/example/board/comment/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.board.comment.entity;
+
+import com.example.board.article.entity.Article;
+import com.example.board.user.entity.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String content;
+
+    @ManyToOne
+    private Article article;
+
+    @ManyToOne
+    private User user;
+}
---
+++ backend/src/main/java/com/example/board/comment/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.example.board.comment.service;
+
+import com.example.board.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+}
