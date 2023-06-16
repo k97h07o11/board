7f3760a280ea3ba073f60c4f2f368ddf10f7e98b
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-06-17 04:38:47 +0900

    :sparkles: 로그인 View 구현

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -4,8 +4,19 @@ function join(data) {
   return http.post(`/users/join`, data);
 }
 
-function login(data) {
-  return http.post(`/users/login`, data);
+function login(username, password) {
+  return http.post(
+    `/users/login`,
+    {
+      grant_type: "authorization_code",
+    },
+    {
+      auth: {
+        username: username,
+        password: password,
+      },
+    }
+  );
 }
 
 export { join, login };
--- frontend/src/views/user/LoginView.vue
+++ frontend/src/views/user/LoginView.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>Login</div>
+  <div>
+    <b-card>
+      <b-form>
+        <b-form-input
+          type="text"
+          id="username"
+          name="username"
+          v-model="username"
+          placeholder="아이디"
+        ></b-form-input>
+
+        <b-form-input
+          type="password"
+          id="password"
+          name="password"
+          v-model="password"
+          placeholder="비밀번호"
+        ></b-form-input>
+
+        <b-button variant="primary" block @click="login">로그인</b-button>
+      </b-form>
+    </b-card>
+  </div>
 </template>
 
 <script>
+import { login } from "@/api/user";
+
 export default {
   name: "LoginView",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login: function () {
+      login(this.username, this.password)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
-
-<style></style>
