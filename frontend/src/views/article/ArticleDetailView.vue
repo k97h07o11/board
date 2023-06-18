8d136d62f56dd3cebff7bccc33585f8c47470009
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-06-19 07:30:33 +0900

    :lipstick: 게시글 조회 View UI 작업

--- frontend/src/views/article/ArticleDetailView.vue
+++ frontend/src/views/article/ArticleDetailView.vue
@@ -5,30 +5,42 @@
         {{ content }}
       </b-card-text>
 
-      <b-button href="#" variant="warning">수정</b-button>
-      <b-button href="#" variant="danger">삭제</b-button>
+      <div v-if="isWriter" class="d-flex justify-content-end gap-3">
+        <b-button href="#" variant="warning">수정</b-button>
+        <b-button href="#" variant="danger">삭제</b-button>
+      </div>
     </b-card>
   </div>
 </template>
 
 <script>
 import { getArticle } from "@/api/article";
+import { mapGetters } from "vuex";
 
 export default {
   name: "ArticleDetailView",
   data() {
     return {
-      title: "",
-      content: "",
+      articleId: null,
+      title: null,
+      content: null,
+      writerId: null,
     };
   },
+  computed: {
+    ...mapGetters(["getUser"]),
+    isWriter() {
+      return this.getUser == this.writerId;
+    },
+  },
   created() {
-    let articleId = this.$route.query.articleId;
-    getArticle(articleId)
+    this.articleId = this.$route.query.articleId;
+    getArticle(this.articleId)
       .then((response) => {
         let data = response.data;
         this.title = data.title;
         this.content = data.content;
+        this.writerId = data.user.id;
       })
       .catch((error) => {
         console.log(error);
@@ -36,3 +48,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.gap-3 {
+  gap: 1rem !important;
+}
+</style>
