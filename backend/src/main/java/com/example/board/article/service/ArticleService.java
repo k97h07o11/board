15287d63aa836a1cd84bb63561ff6b9205023e0e
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-06-19 07:30:46 +0900

    :sparkles: 게시글 삭제 기능 구현

--- backend/src/main/java/com/example/board/article/service/ArticleService.java
+++ backend/src/main/java/com/example/board/article/service/ArticleService.java
@@ -2,12 +2,14 @@ package com.example.board.article.service;
 
 import com.example.board.article.entity.Article;
 import com.example.board.article.repository.ArticleRepository;
+import com.example.board.comment.repository.CommentRepository;
 import com.example.board.user.entity.User;
 import com.example.board.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -16,6 +18,8 @@ import java.util.NoSuchElementException;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+
+    private final CommentRepository commentRepository;
 
     private final UserRepository userRepository;
 
@@ -42,7 +46,9 @@ public class ArticleService {
         articleRepository.save(article);
     }
 
+    @Transactional
     public void deleteArticle(Long articleId) {
+        commentRepository.deleteAllByArticleId(articleId);
         articleRepository.deleteById(articleId);
     }
 }
