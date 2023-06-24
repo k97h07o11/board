13258cc5ba1491cb8ab6e292f82c9226f818da24
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-06-24 22:19:26 +0900

    :sparkles: 댓글 작성, 수정 요청 유효성 검사 추가

--- backend/src/main/java/com/example/board/comment/controller/CommentController.java
+++ backend/src/main/java/com/example/board/comment/controller/CommentController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -25,7 +26,7 @@ public class CommentController {
     @PostMapping("/articles/{articleId}/comments")
     public void postComment(
             @PathVariable Long articleId,
-            @RequestBody CommentRequestDto commentRequestDto,
+            @RequestBody @Valid CommentRequestDto commentRequestDto,
             Authentication authentication
     ) {
         Long userId = Long.parseLong(authentication.getName());
@@ -35,7 +36,7 @@ public class CommentController {
     @PutMapping("/comments/{commentId}")
     public void putComment(
             @PathVariable Long commentId,
-            @RequestBody CommentRequestDto commentRequestDto
+            @RequestBody @Valid CommentRequestDto commentRequestDto
     ) {
         commentService.editComment(commentId, commentRequestDto);
     }
--- backend/src/main/java/com/example/board/comment/dto/CommentRequestDto.java
+++ backend/src/main/java/com/example/board/comment/dto/CommentRequestDto.java
@@ -3,9 +3,12 @@ package com.example.board.comment.dto;
 import com.example.board.comment.entity.Comment;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class CommentRequestDto {
 
+    @NotBlank
     private String content;
 
     public Comment toEntity() {
